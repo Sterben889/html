5cdc1342176f483470f0b7f1c7496f3ca9b9cc54
Sterben889 <59096329+Sterben889@users.noreply.github.com>
2022-10-12 22:12:53 -0600

    Added mp3 file to played after timer. Timer actually works now

--- FinalCountdown.js
+++ FinalCountdown.js
@@ -1,8 +1,8 @@
-const endDate = new Date("October 13, 2022 UTC-6")
+const endDate = new Date("December 8, 2022 UTC-6")
 let previousTimeBetweenDates
 setInterval(() => {
   const currentDate = new Date()
-  const timeBetweenDates = Math.ceil((endDate - currentDate + currentDate.getTimezoneOffset()) / 1000)
+  const timeBetweenDates = Math.ceil((endDate - currentDate) / 1000)
   if (previousTimeBetweenDates !== timeBetweenDates) {
     flipAllCards(timeBetweenDates)
   }
@@ -11,12 +11,21 @@ setInterval(() => {
 }, 250)
 
 function flipAllCards(time) {
+  var audio = new Audio('FinalCountdown.mp3#t=00:01:58');
+  if (time == 0) {
+   audio.addEventListener("canplaythrough", () => {
+   audio.play().catch(e => {
+      window.addEventListener('click', () => {
+         audio.play()
+      }, { once: true })
+   })
+});
+  }
+  if (time < 0) return
   const seconds = time % 60
   const minutes = Math.floor(time / 60) % 60
   const hours = Math.floor(time / 3600) % 24
   let days = Math.floor(time / 86400)
-  console.log(days)
-
  flip(document.querySelector("[data-days-hundreds]"), Math.floor(days / 100))
  flip(document.querySelector("[data-days-tens]"), Math.floor(days / 10 % 10))
  flip(document.querySelector("[data-days-ones]"), days % 10)
@@ -26,7 +35,11 @@ function flipAllCards(time) {
  flip(document.querySelector("[data-minutes-ones]"), minutes % 10)
  flip(document.querySelector("[data-seconds-tens]"), Math.floor(seconds / 10))
  flip(document.querySelector("[data-seconds-ones]"), seconds % 10)
+
+
 }
+
+
 
 function flip(flipCard, newNumber) {
   const tophalf = flipCard.querySelector(".top")
